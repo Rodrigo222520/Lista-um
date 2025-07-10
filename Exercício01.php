ad0de9c7ebbd8a9a275bc7f43ae915072e069a7e
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-07-10 10:03:36 -0300

    Exercício 1 concluído, fiz de um jeito quase certo, mas acabei não entendo o que eu estava errando.

---
+++ Exercício01.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Verificar se o número é par ou ímpar</title>
+</head>
+<body>
+
+<form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_par_impar">Verificar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['verificar_par_impar'])) {
+            $numero = $_POST['numero'];
+            if ($numero % 2 == 0) {
+                echo "O número $numero é <strong>par</strong>.";
+            } else {
+                echo "O número $numero é <strong>ímpar</strong>.";
+            }
+        }
+    }
+    ?>
+
+    
+</body>
+</html>
